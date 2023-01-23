a1aba1cfef860e2db760e0ac8508bdaaa37c0d93
LCGuedes <lucasmainguedes@gmail.com>
2023-01-23 16:33:08 -0300

    test: ensure SignUpController returns status 400 if confirmPassword fails

--- server-app/src/presentation/controllers/signUp.spec.ts
+++ server-app/src/presentation/controllers/signUp.spec.ts
@@ -26,4 +26,16 @@ describe("SignUp", () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
   });
+  it("Should return status 400 if confirmPassword fails", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        username: "any_username",
+        password: "any_pwd",
+        confirmPassword: "any_different_pwd",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
 });
