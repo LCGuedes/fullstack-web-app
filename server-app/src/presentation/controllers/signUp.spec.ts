28e6be57b10598f9cd0b61a81950d337657b3915
LCGuedes <lucasmainguedes@gmail.com>
2023-01-23 15:04:00 -0300

    test: ensure SignUpController return status 400 if name is not provided

---
+++ server-app/src/presentation/controllers/signUp.spec.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from "vitest";
+import { SignUpController } from "./signUp";
+
+describe("SignUp", () => {
+  it("Should return status 400 if name is not provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      password: "any_pwd",
+      confirmPassword: "any_confirmPwd",
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
