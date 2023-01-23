ecca8bbc29685dc16effbb59ce31e959bc4add87
LCGuedes <lucasmainguedes@gmail.com>
2023-01-23 15:24:03 -0300

    feat: ensure SignUpController returns status 400 if confirmPassword is not provided

--- server-app/src/presentation/controllers/signUp.ts
+++ server-app/src/presentation/controllers/signUp.ts
@@ -1,7 +1,8 @@
 export class SignUpController {
   handle(httpRequest: any): any {
-    const { username, password } = httpRequest;
+    const { username, password, confirmPassword } = httpRequest;
     if (!username) return { statusCode: 400 };
     if (!password) return { statusCode: 400 };
+    if (!confirmPassword) return { statusCode: 400 };
   }
 }
