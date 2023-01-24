bf394aaefb1448408a86e1ee0f412646016fcdca
LCGuedes <lucasmainguedes@gmail.com>
2023-01-24 10:17:24 -0300

    refactor: move sut and encrypterStub to a factory helper method

--- server-app/src/data/useCases/dbAddAccount.ts/dbAddAccount.spec.ts
+++ server-app/src/data/useCases/dbAddAccount.ts/dbAddAccount.spec.ts
@@ -1,16 +1,31 @@
 import { describe, expect, it, vi } from "vitest";
+import { Encrypter } from "../../protocols/encrypter";
 import { DbAddAccount } from "./dbAddAccount";
+
+const makeEncrypterStub = () => {
+  class EncrypterStub implements Encrypter {
+    encrypt(password: string): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_password"));
+    }
+  }
+  return new EncrypterStub();
+};
+
+interface sutTypes {
+  encrypterStub: Encrypter;
+  sut: DbAddAccount;
+}
+
+const makeSut = (): sutTypes => {
+  const encrypterStub = makeEncrypterStub();
+  const sut = new DbAddAccount(encrypterStub);
+  return { sut, encrypterStub };
+};
 
 describe("DbAddAccount", () => {
   it("Should call Encrypter with correct password", async () => {
-    class EncrypterStub {
-      encrypt(password: string): Promise<string> {
-        return new Promise((resolve) => resolve("hashed_password"));
-      }
-    }
-    const encrypterStub = new EncrypterStub();
+    const { sut, encrypterStub } = makeSut();
     const encryptSpy = vi.spyOn(encrypterStub, "encrypt");
-    const sut = new DbAddAccount(encrypterStub);
     const accountData = {
       username: "valid_username",
       password: "valid_password",
