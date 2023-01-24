46373aa44f4d9bffa60681ff15bc01d820768f8b
LCGuedes <lucasmainguedes@gmail.com>
2023-01-24 12:11:30 -0300

    test: ensure DbAccount calls AddAccountRepository with correct values

--- server-app/src/data/useCases/dbAddAccount.ts/dbAddAccount.spec.ts
+++ server-app/src/data/useCases/dbAddAccount.ts/dbAddAccount.spec.ts
@@ -1,6 +1,24 @@
 import { describe, expect, it, vi } from "vitest";
+import { AccountModel } from "../../../domain/models/account";
+import { AddAccountModel } from "../../../domain/useCases/addAccount";
 import { Encrypter } from "../../protocols/encrypter";
 import { DbAddAccount } from "./dbAddAccount";
+
+import { AddAccountRepository } from "../../protocols/addAccountRepository";
+
+const makeAddAccountRepositoryStub = () => {
+  class AddAccountRepositoryStub implements AddAccountRepository {
+    add(account: AddAccountModel): Promise<AccountModel> {
+      const fakeAccount = {
+        id: "valid_id",
+        username: "valid_username",
+        password: "hashed_password",
+      };
+      return new Promise((resolve) => resolve(fakeAccount));
+    }
+  }
+  return new AddAccountRepositoryStub();
+};
 
 const makeEncrypterStub = () => {
   class EncrypterStub implements Encrypter {
@@ -12,14 +30,16 @@ const makeEncrypterStub = () => {
 };
 
 interface sutTypes {
+  addAccountRepositoryStub: AddAccountRepository;
   encrypterStub: Encrypter;
   sut: DbAddAccount;
 }
 
 const makeSut = (): sutTypes => {
+  const addAccountRepositoryStub = makeAddAccountRepositoryStub();
   const encrypterStub = makeEncrypterStub();
-  const sut = new DbAddAccount(encrypterStub);
-  return { sut, encrypterStub };
+  const sut = new DbAddAccount(encrypterStub, addAccountRepositoryStub);
+  return { sut, encrypterStub, addAccountRepositoryStub };
 };
 
 describe("DbAddAccount", () => {
@@ -45,5 +65,21 @@ describe("DbAddAccount", () => {
     };
     const promise = sut.add(accountData);
     await expect(promise).rejects.toThrow();
+  });
+
+  it("Should call AddAccountRepository with correct values", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+
+    const addSpy = vi.spyOn(addAccountRepositoryStub, "add");
+
+    const accountData = {
+      username: "valid_username",
+      password: "valid_password",
+    };
+    await sut.add(accountData);
+    expect(addSpy).toBeCalledWith({
+      username: "valid_username",
+      password: "hashed_password",
+    });
   });
 });
